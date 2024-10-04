688309e29ffed1e475ce1d68e0a9c0b325e744f1
David Hall <davidsmonkeys@hotmail.com>
2024-10-04 16:40:32 +0100

    Update environment.ts
    Again set for the app. Changes to be for web

--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -7,7 +7,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:44100/api',
-  auth0CallbackUrl: "http://localhost:44300",
+  auth0CallbackUrl: "http://localhost:44200",
   allowedDomains: [
     "localhost:44100"
   ]
